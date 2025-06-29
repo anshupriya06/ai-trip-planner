e39608892343ea84f62c0ab0e2b7b357cddfb5ed
Anshu Priya <anshupriyadnr06@gmail.com>
2025-06-29 10:44:05 +0530

    tailwind setup done

--- ai-trip-planner/src/App.jsx
+++ ai-trip-planner/src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import { Button } from "@/components/ui/button";
+
+
 
 function App() {
-  
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
