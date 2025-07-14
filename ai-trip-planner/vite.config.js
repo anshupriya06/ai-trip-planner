cbc419443c1993c3728db9fc1ff07d2fe22dc7ff
Anshu Priya <anshupriyadnr06@gmail.com>
2025-07-14 20:08:13 +0530

    shadcn ui setup done

--- ai-trip-planner/vite.config.js
+++ ai-trip-planner/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { fileURLToPath } from "url"
+import { dirname } from "path"
+import { defineConfig } from "vite"
+
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
